e20c5631ffca1901e92f367d5e5c239982c6fd6d
Johnathon Schultz <johnathon.schultz@gmail.com>
2015-08-15 11:57:58 -0700

    Initial commit

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,24 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 4)
+    assert KV.Bucket.get(bucket, "eggs") == 4
+
+    val = KV.Bucket.delete(bucket, "eggs")
+    assert KV.Bucket.get(bucket, "eggs") == nil
+    assert val == 4
+  end
+end
